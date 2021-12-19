a452d6cc19f33e79f87565f235fce91b037b58ea
Minhdang04 <80468760+Minhdang04@users.noreply.github.com>
2021-12-19 11:43:26 +0700

    Add files via upload

---
+++ cuoikhoa.js
@@ -0,0 +1,99 @@
+var searcharr = [{ name: "" }, { name: "OPM" }, { name: "DB" }, { name: "KnY" }, { name: "ReZero" }];
+var imgarr = [
+    {
+      nameid: "ReZero",
+      img: "https://i.imgur.com/BFLzXKf.jpeg",
+    },
+    {
+        nameid: "ReZero",
+        img: "https://images6.fanpop.com/image/photos/39700000/Subaru-Rem-Emilia-re-zero-39761423-3166-4082.jpg",
+      },
+      {
+        nameid: "ReZero",
+        img: "https://i.imgur.com/LXieILe.jpeg",
+      },
+      {
+        nameid: "ReZero",
+        img: "https://i.imgur.com/fTfO78x.gif",
+      },
+      {
+        nameid: "ReZero",
+        img: "https://i.imgur.com/xhK740a.gif",
+      },
+      {
+        nameid: "ReZero",
+        img: "https://i.imgur.com/H3M3DMM.mp4",
+      },
+      {
+        nameid: "ReZero",
+        img: "https://i.imgur.com/MQXE703.mp4",
+      },
+      {
+        nameid: "DB",
+        img: "https://images6.fanpop.com/image/photos/35300000/-Gogeeta-dragon-ball-z-35367836-490-276.gif",
+      },
+      {
+        nameid: "DB",
+        img: "https://images6.fanpop.com/image/photos/35700000/Vegeta-dragon-ball-z-35799481-762-1048.jpg",
+      },
+      {
+        nameid: "DB",
+        img: "https://i.imgur.com/itE3Svq.gif",
+      },
+      {
+        nameid: "OPM",
+        img: "https://i.imgur.com/eaxENje.jpeg",
+      },
+      {
+        nameid: "OPM",
+        img: "https://i.imgur.com/V7Y2Yq2.jpeg",
+      },
+      {
+        nameid: "OPM",
+        img: "https://i.imgur.com/LV7H0EY.mp4",
+      },
+      {
+        nameid: "OPM",
+        img: "https://i.imgur.com/b9YK6Gc.jpeg",
+      },
+      {
+        nameid: "OPM",
+        img: "https://images6.fanpop.com/image/photos/43600000/Senritsu-no-Tatsumaki-one-punch-man-43676456-300-416.jpg",
+      },
+      {
+        nameid: "OPM",
+        img: "https://images6.fanpop.com/image/photos/43600000/Genos-one-punch-man-43631244-300-445.jpg",
+      },
+      {
+        nameid: "OPM",
+        img: "https://images6.fanpop.com/image/photos/43900000/monster-garou-one-punch-man-43970707-899-1431.jpg",
+      },
+      {
+        nameid: "KnY",
+        img: "https://images6.fanpop.com/image/photos/43700000/-Kyojuro-Rengoku-kimetsu-no-yaiba-43779242-220-193.gif",
+      },
+      {
+        nameid: "KnY",
+        img: "https://www.fanpop.com/clubs/kimetsu-no-yaiba/images/43557737/title/yoriichi-kokushibo-photo?card",
+      },
+      {
+        nameid: "KnY",
+        img: "https://images6.fanpop.com/image/photos/43500000/Descendants-Muichiro-Tokito-Tanjirou-kamado-kimetsu-no-yaiba-43555903-300-370.jpg",
+      },
+      {
+        nameid: "KnY",
+        img: "https://i.imgur.com/k08VkU3.mp4",
+      },
+      {
+        nameid: "KnY",
+        img: "https://i.imgur.com/xjZsquw.mp4",
+      },
+      {
+        nameid: "KnY",
+        img: "https://images6.fanpop.com/image/photos/43400000/Inosuke-Hashibira-kimetsu-no-yaiba-43408744-500-281.gif",
+      },
+      {
+        nameid: "KnY",
+        img: "https://fandom.vn/wp-content/uploads/2019/09/S%C3%A1t-qu%E1%BB%B7-%C4%91%E1%BB%99i.png",
+      },
+    ]
